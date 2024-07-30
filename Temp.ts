1be908de34df9d9261114194381a744420d6d58e
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-07-30 10:22:01 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,108 +1,50 @@
-// genericServiceProvider.ts
-
-type FetchOptions = {
-  method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
-  headers?: HeadersInit;
-  body?: any;
-};
-
-type FetchCallbacks<T> = {
-  onSuccess: (data: T) => void;
-  onError: (error: any) => void;
-  onInterrupt?: () => void;
-};
-
-class GenericServiceProvider {
-  private abortControllers: Map<string, AbortController>;
-
-  constructor() {
-    this.abortControllers = new Map();
-  }
-
-  private createAbortController(key: string): AbortController {
-    const abortController = new AbortController();
-    this.abortControllers.set(key, abortController);
-    return abortController;
-  }
-
-  private removeAbortController(key: string): void {
-    this.abortControllers.delete(key);
-  }
-
-  public async fetchService<T>(
-    url: string,
-    options: FetchOptions,
-    callbacks: FetchCallbacks<T>,
-    interruptKey?: string
-  ): Promise<void> {
-    const controller = interruptKey ? this.createAbortController(interruptKey) : new AbortController();
-    const { signal } = controller;
-
-    try {
-      const response = await fetch(url, { ...options, signal });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data: T = await response.json();
-      callbacks.onSuccess(data);
-    } catch (error) {
-      if (signal.aborted) {
-        callbacks.onInterrupt?.();
-      } else {
-        callbacks.onError(error);
-      }
-    } finally {
-      if (interruptKey) {
-        this.removeAbortController(interruptKey);
-      }
-    }
-  }
-
-  public interruptFetch(key: string): void {
-    const controller = this.abortControllers.get(key);
-    if (controller) {
-      controller.abort();
-    }
-  }
-}
-
-export default GenericServiceProvider;
-
-import React, { useEffect } from 'react';
 import GenericServiceProvider from './genericServiceProvider';
 
-const MyComponent: React.FC = () => {
+const systemLoader = (window as any).System;
+
+/* Single SPA load function */
+
+export const loadApp = (url: string, appName: string) => async () => {
+  const manifestUrl = url + '/manifest.json';
+  let mainJS;
+
+  const getFilename = (data) => {
+    const fileobj = data?.files?.scripts?.find((script: any) => script.fileName && script.fileName.indexOf('main') !== -1);
+    return fileobj?.fileName;
+  };
+
+  const sessionBuildIntData = window.sessionStorage.getItem('manifestData');
+  const data = sessionBuildIntData && JSON.parse(sessionBuildIntData)[appName];
+
   const serviceProvider = new GenericServiceProvider();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await serviceProvider.fetchService<any>(
-        'https://api.example.com/data',
-        { method: 'GET' },
-        {
-          onSuccess: (data) => {
-            console.log('Data fetched successfully:', data);
-          },
-          onError: (error) => {
-            console.error('Error fetching data:', error);
-          },
-          onInterrupt: () => {
-            console.log('Fetch request interrupted.');
-          }
+  if (data) {
+    mainJS = `${url}/${getFilename(data)}`;
+  } else {
+    await serviceProvider.fetchService<any>(
+      manifestUrl,
+      { method: 'GET', headers: { 'Cache-Control': 'no-cache' } },
+      {
+        onSuccess: (data) => {
+          const intData = sessionBuildIntData ? JSON.parse(sessionBuildIntData) : {};
+          intData[appName] = data;
+          intData[appName]['description'] = url;
+          window.sessionStorage.setItem('manifestData', JSON.stringify(intData));
+          mainJS = `${url}/${getFilename(data)}`;
         },
-        'fetchDataKey'
-      );
-    };
+        onError: (err) => {
+          console.error('Error in fetching data', err);
+          mainJS = '';
+        }
+      }
+    );
+  }
 
-    fetchData();
+  const app = await systemLoader?.import(mainJS);
 
-    return () => {
-      serviceProvider.interruptFetch('fetchDataKey');
-    };
-  }, [serviceProvider]);
-
-  return <div>My Component</div>;
+  if (app?.useDefault) {
+    return app.default;
+  } else {
+    return app;
+  }
 };
-
-export default MyComponent;
-
