352b6fa3c947b4e666c32451f9da8d42d02367ba
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-08-01 12:34:38 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,77 +1,25 @@
-// src/DynamicForm.tsx
-import React from 'react';
-import { useFormContext } from './FormProvider';
+// src/FormProvider.tsx
+import React, { createContext, useContext, useEffect } from 'react';
+import { useForm, FormProvider as RHFProvider } from 'react-hook-form';
 import { useConfigContext } from './ConfigProvider';
 
-interface FieldProps {
-  field: any;
-  register: any;
-  error: any;
-}
+const FormContext = createContext<any>(null);
 
-interface DynamicFormProps {
-  onSubmit: (data: any) => void;
-  onError: (errors: any) => void;
-  customFieldComponents?: { [key: string]: React.FC<FieldProps> };
-  onClickBack?: () => void;
-  clearForm?: () => void;
-}
+export const useFormContext = () => useContext(FormContext);
 
-const DefaultField: React.FC<FieldProps> = ({ field, register, error }) => (
-  <div>
-    <label htmlFor={field.name}>{field.label}</label>
-    <input
-      id={field.name}
-      {...register(field.name, field.validation)}
-      type={field.type}
-      defaultValue={field.defaultValue}
-      aria-invalid={!!error}
-      aria-describedby={`${field.name}-error`}
-    />
-    {error && <span id={`${field.name}-error`}>{error.message}</span>}
-  </div>
-);
+export const FormProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { config } = useConfigContext();
+  const methods = useForm();
 
-const DynamicForm: React.FC<DynamicFormProps> = ({ onSubmit, onError, customFieldComponents = {}, onClickBack, clearForm }) => {
-  const { config, loading, error } = useConfigContext();
-  const { register, handleSubmit, reset, formState: { errors } } = useFormContext();
-
-  const handleClearForm = () => {
+  useEffect(() => {
     if (config) {
-      reset(config.defaultValues);
+      methods.reset(config.defaultValues);
     }
-  };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  if (!config) {
-    return <div>No configuration found</div>;
-  }
+  }, [config, methods]);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit, onError)} noValidate>
-      {config.fields.map((field) => {
-        const FieldComponent = customFieldComponents[field.type] || DefaultField;
-        return (
-          <FieldComponent
-            key={field.name}
-            field={field}
-            register={register}
-            error={errors[field.name]}
-          />
-        );
-      })}
-      <button type="submit">Submit</button>
-      {onClickBack && <button type="button" onClick={onClickBack}>Back</button>}
-      {clearForm && <button type="button" onClick={handleClearForm}>Clear</button>}
-    </form>
+    <FormContext.Provider value={methods}>
+      <RHFProvider {...methods}>{children}</RHFProvider>
+    </FormContext.Provider>
   );
 };
-
-export default DynamicForm;
