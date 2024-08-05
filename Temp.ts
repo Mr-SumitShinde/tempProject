ea11278ac3a91a7ae7de61c5ad79c5bd1906ecea
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-08-05 11:21:17 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -27,20 +27,69 @@ const createApp = (appName) => {
   executeCommand('npx create-react-app .');
 
   console.log('Installing additional dependencies...');
-  // Add your additional dependencies here
+  // Add your additional dependencies with specific versions here
   const dependencies = [
-    'blueprintjs/core',
-    'blueprintjs/icons',
-    '@emotion/react',
-    '@emotion/styled',
-    'axios'
+    'react@18.2.0',
+    'react-dom@18.2.0',
+    '@ag-grid-community/client-side-row-model@^31.3.2',
+    '@ag-grid-community/core@^31.3.2',
+    '@ag-grid-community/react@^31.3.2',
+    '@barclays/blueprint-fonts-effra@^0.63.0',
+    '@barclays/blueprint-fonts-expert-sans@^0.63.0',
+    'ag-grid-react@^31.3.2',
+    'react-hook-form@^7.51.5',
+    'react-router-dom@6.11.2',
+    'tslib@^2.3.0'
   ];
   executeCommand(`npm install ${dependencies.join(' ')}`);
 
   const devDependencies = [
-    'jest',
-    '@testing-library/react',
-    '@testing-library/jest-dom'
+    'postcss-nesting@^12.1.4',
+    '@babel/core@^7.14.5',
+    '@babel/preset-react@^7.14.5',
+    '@barclays/blueprint-icons@^0.63.0',
+    '@barclays/blueprint-react@^0.63.0',
+    '@nx/eslint@18.1.3',
+    '@nx/eslint-plugin@18.1.3',
+    '@nx/jest@18.1.3',
+    '@nx/js@18.1.3',
+    '@nx/react@^18.1.3',
+    '@nx/vite@18.1.3',
+    '@nx/web@18.1.3',
+    '@swc-node/register@~1.8.0',
+    '@swc/cli@~0.1.62',
+    '@swc/core@~1.3.85',
+    '@swc/helpers@~0.5.2',
+    '@testing-library/react@14.0.0',
+    '@types/jest@^29.4.0',
+    '@types/node@18.16.9',
+    '@types/react@18.2.33',
+    '@types/react-dom@18.2.14',
+    '@typescript-eslint/eslint-plugin@^6.13.2',
+    '@typescript-eslint/parser@^6.13.2',
+    '@vitejs/plugin-react@^4.2.0',
+    '@vitest/ui@^1.3.1',
+    'babel-jest@^29.4.1',
+    'eslint@~8.48.0',
+    'eslint-config-prettier@^9.0.0',
+    'eslint-plugin-import@2.27.5',
+    'eslint-plugin-jsx-a11y@6.7.1',
+    'eslint-plugin-react@7.32.2',
+    'eslint-plugin-react-hooks@4.6.0',
+    'jest@^29.4.1',
+    'jest-environment-jsdom@^29.4.1',
+    'jest-environment-node@^29.4.1',
+    'jsdom@~22.1.0',
+    'nx@18.1.3',
+    'prettier@^2.6.2',
+    'sass@^1.55.0',
+    'swc-loader@0.1.15',
+    'ts-jest@^29.1.0',
+    'ts-node@10.9.1',
+    'typescript@~5.3.2',
+    'vite@~5.0.0',
+    'vite-plugin-dts@~2.3.0',
+    'vitest@^1.3.1'
   ];
   executeCommand(`npm install --save-dev ${devDependencies.join(' ')}`);
 
@@ -59,19 +108,3 @@ if (!appName) {
 }
 
 createApp(appName);
-
-{
-  "name": "create-clm-app",
-  "version": "1.0.0",
-  "description": "CLI to create a new CLM app",
-  "main": "index.js",
-  "bin": {
-    "create-clm-app": "./index.js"
-  },
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "dependencies": {
-    "fs": "^0.0.1-security"
-  }
-}
