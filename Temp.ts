c98009edf5ad6a1143b8c0a4f70b85d20a174d26
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-08-21 17:58:09 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,1 +1,30 @@
-npm install --save-dev webpack webpack-cli webpack-node-externals babel-loader @babel/core @babel/preset-env @babel/preset-react
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.js', // Entry point of your React app
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'index.js', // Output file name
+    libraryTarget: 'commonjs2', // Export as a CommonJS module
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Transpile JS/JSX files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/, // Optionally handle CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  externals: [nodeExternals()], // Exclude node_modules from the bundle
+  resolve: {
+    extensions: ['.js', '.jsx'], // Resolve these file extensions
+  },
+};
