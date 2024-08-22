50b8ba8436051e43f8849a6cff3d75165cea8a04
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-08-22 12:03:42 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,37 +1,46 @@
-npm install --save-dev @svgr/webpack
-
-
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx', // Adjust the entry point based on your project structure
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    filename: 'bundle.js',
+    library: 'MyPackage', // Replace with your package name
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.tsx?$/,
+        use: 'babel-loader',
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.svg$/, // Handle SVG files
-        use: ['@svgr/webpack'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
-  externals: [nodeExternals()],
-  resolve: {
-    extensions: ['.js', '.jsx'],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
+  devtool: 'source-map',
 };
