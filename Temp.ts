ace71e25a48aeab48c44da2aca2adda2327c56dc
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-08-23 11:56:13 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,52 +1,20 @@
-const { NxReactWebpackPlugin } = require('@nrwl/react/plugins/webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.esm.js',
-    library: {
-      type: 'module',
-    },
-    module: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new NxReactWebpackPlugin(),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
-  devtool: 'source-map',
-  experiments: {
-    outputModule: true,
-  },
-};
+{
+  "root": "apps/my-app",
+  "sourceRoot": "apps/my-app/src",
+  "projectType": "application",
+  "targets": {
+    "build": {
+      "executor": "@nrwl/web:build",
+      "outputs": ["{options.outputPath}"],
+      "options": {
+        "outputPath": "dist/apps/my-app",
+        "index": "apps/my-app/src/index.html",
+        "main": "apps/my-app/src/main.tsx",
+        "tsConfig": "apps/my-app/tsconfig.app.json",
+        "assets": ["apps/my-app/src/favicon.ico", "apps/my-app/src/assets"],
+        "styles": ["apps/my-app/src/styles.css"],
+        "scripts": []
+      }
+    }
+  }
+}
