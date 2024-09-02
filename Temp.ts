76c60eb111988951d607babc2c26623dea3cb417
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-09-02 18:04:35 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,36 +1,81 @@
-export interface ValpreAPIConfig extends RequestInit {
-    url?: string;
-    baseURL?: string;
-    params?: Record<string, any>;
-    headers?: Record<string, string>;
-    timeout?: number;
-    withCredentials?: boolean;
-    xsrfCookieName?: string;
-    xsrfHeaderName?: string;
-    cancelToken?: {
-        signal: AbortSignal;
+import { httpAdapter } from './httpAdapter';
+import { ValpreAPIConfig, defaults } from '../config';
+import { InterceptorManager } from '../interceptors';
+import { CancelToken } from '../cancelToken';
+import * as Methods from './methods';
+import * as InstanceMethods from './instanceMethods';
+import * as UtilityMethods from './utilityMethods';
+
+export class ValpreAPI {
+    defaults: ValpreAPIConfig;
+    interceptors: {
+        request: InterceptorManager<ValpreAPIConfig>;
+        response: InterceptorManager<Response>;
     };
-    onUploadProgress?: (progressEvent: ProgressEvent) => void;
-    onDownloadProgress?: (progressEvent: ProgressEvent) => void;
-    responseType?: 'json' | 'blob' | 'text' | 'arrayBuffer';
-    retries?: number;
-    retryDelay?: number;
-    retryCondition?: (error: any, attempt: number) => boolean;
-    adapter?: (config: ValpreAPIConfig) => Promise<Response>;
-}
+    private adapter: (config: ValpreAPIConfig) => Promise<Response>;
 
-export const defaults: ValpreAPIConfig = {
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
-    method: 'GET',
-    timeout: 0, // Default timeout is 0 (no timeout)
-    withCredentials: false,
-    xsrfCookieName: 'XSRF-TOKEN',
-    xsrfHeaderName: 'X-XSRF-TOKEN'
-};
+    constructor(config: ValpreAPIConfig = {}, adapter?: (config: ValpreAPIConfig) => Promise<Response>) {
+        this.defaults = { ...defaults, ...config };
+        this.adapter = adapter || httpAdapter;
+        this.interceptors = {
+            request: new InterceptorManager<ValpreAPIConfig>(),
+            response: new InterceptorManager<Response>(),
+        };
+    }
 
-export function setDefaults(newDefaults: Partial<ValpreAPIConfig>): void {
-    Object.assign(defaults, newDefaults);
+    request(config: ValpreAPIConfig): Promise<Response> {
+        return this.adapter(config);
+    }
+
+    get(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
+        return InstanceMethods.instanceGet.call(this, url, config);
+    }
+
+    post(url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
+        return InstanceMethods.instancePost.call(this, url, data, config);
+    }
+
+    put(url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
+        return InstanceMethods.instancePut.call(this, url, data, config);
+    }
+
+    delete(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
+        return InstanceMethods.instanceDelete.call(this, url, config);
+    }
+
+    patch(url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
+        return InstanceMethods.instancePatch.call(this, url, data, config);
+    }
+
+    head(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
+        return InstanceMethods.instanceHead.call(this, url, config);
+    }
+
+    options(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
+        return InstanceMethods.instanceOptions.call(this, url, config);
+    }
+
+    static setDefaults(newDefaults: Partial<ValpreAPIConfig>): void {
+        UtilityMethods.setGlobalDefaults(newDefaults);
+    }
+
+    static create(instanceConfig: ValpreAPIConfig): ValpreAPI {
+        return UtilityMethods.createInstance(instanceConfig);
+    }
+
+    static CancelToken = CancelToken;
+
+    static isValpreAPIError(error: any): error is ValpreAPIError {
+        return UtilityMethods.isValpreAPIError(error);
+    }
+
+    static all = (promises: Array<Promise<any>>): Promise<any[]> => {
+        return Promise.all(promises);
+    };
+
+    static spread = (callback: Function): (arr: any[]) => any => {
+        return function wrap(arr: any[]) {
+            return callback(...arr);
+        };
+    };
 }
