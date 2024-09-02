408cd15807bf82011558d45a52a59b34014c6deb
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-09-02 17:35:49 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,33 +1,27 @@
-import { ValpreAPI } from './valpreAPI';
+import { httpAdapter } from './httpAdapter';
+import { ValpreAPIConfig, defaults } from '../config';
+import { InterceptorManager } from '../interceptors';
+import { CancelToken } from '../cancelToken';
+import * as Methods from './methods';
+import * as InstanceMethods from './instanceMethods';
+import * as AxiosLikeMethods from './axiosLikeMethods';
 
-export function instanceRequest(this: ValpreAPI, config: ValpreAPIConfig): Promise<Response> {
-    return this.request(config);
-}
+export class ValpreAPI {
+    defaults: ValpreAPIConfig;
+    interceptors: {
+        request: InterceptorManager<ValpreAPIConfig>;
+        response: InterceptorManager<Response>;
+    };
+    private adapter: (config: ValpreAPIConfig) => Promise<Response>;
 
-export function instanceGet(this: ValpreAPI, url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-    return this.get(url, config);
-}
+    constructor(config: ValpreAPIConfig = {}, adapter?: (config: ValpreAPIConfig) => Promise<Response>) {
+        this.defaults = { ...defaults, ...config };
+        this.adapter = adapter || httpAdapter;
+        this.interceptors = {
+            request: new InterceptorManager<ValpreAPIConfig>(),
+            response: new InterceptorManager<Response>(),
+        };
+    }
 
-export function instancePost(this: ValpreAPI, url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
-    return this.post(url, data, config);
-}
-
-export function instancePut(this: ValpreAPI, url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
-    return this.put(url, data, config);
-}
-
-export function instanceDelete(this: ValpreAPI, url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-    return this.delete(url, config);
-}
-
-export function instancePatch(this: ValpreAPI, url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
-    return this.patch(url, data, config);
-}
-
-export function instanceHead(this: ValpreAPI, url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-    return this.head(url, config);
-}
-
-export function instanceOptions(this: ValpreAPI, url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-    return this.options(url, config);
-}
+    request(config: ValpreAPIConfig): Promise<Response> {
+        return this
