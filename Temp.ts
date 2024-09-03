2c73084a8a5e5d9413e184c1a23037ff4bbd3d24
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-09-03 09:06:11 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,107 +1,44 @@
-import { httpAdapter } from './httpAdapter';
-import { ValpreAPIConfig, defaults } from './config';
-import { InterceptorManager } from './interceptors';
-import { CancelToken } from './cancelToken';
-import * as InstanceMethods from './instanceMethods';
-import * as UtilityMethods from './utilityMethods';
-import { applyCSRFToken } from './utils/csurf';
-import { addRetryCapability } from './utils/retry';
-import { sendRequestWithProgress } from './utils/progress';
-import { handleRequestData, handleResponseData } from './utils/jsonHandling';
-import { isFormData, handleFormData } from './utils/formData';
-
-export class ValpreAPI {
-    defaults: ValpreAPIConfig;
-    interceptors: {
-        request: InterceptorManager<ValpreAPIConfig>;
-        response: InterceptorManager<Response>;
-    };
-    private adapter: (config: ValpreAPIConfig) => Promise<Response>;
-
-    constructor(config: ValpreAPIConfig = {}, adapter?: (config: ValpreAPIConfig) => Promise<Response>) {
-        this.defaults = { ...defaults, ...config };
-        this.adapter = adapter || httpAdapter;
-        this.interceptors = {
-            request: new InterceptorManager<ValpreAPIConfig>(),
-            response: new InterceptorManager<Response>(),
-        };
+export function handleRequestData(
+    data: any, 
+    headers: Record<string, string>,
+    transformRequest?: (data: any, headers: Record<string, string>) => any
+): string | undefined {
+    if (transformRequest) {
+        return transformRequest(data, headers);
     }
+    if (typeof data === 'object' && data !== null) {
+        return JSON.stringify(data);
+    }
+    return data;
+}
 
-    async request(config: ValpreAPIConfig): Promise<Response> {
-        // Apply defaults and interceptors
-        config = { ...this.defaults, ...config };
-        config = await this.interceptors.request.run(config);
+export async function handleResponseData(
+    response: Response, 
+    responseType?: string,
+    transformResponse?: (data: any) => any
+): Promise<any> {
+    const contentType = response.headers.get('Content-Type') || '';
+    let data: any;
 
-        // Apply CSRF token if necessary
-        applyCSRFToken(config);
-
-        // Handle form data or JSON data
-        if (isFormData(config.body)) {
-            config.body = handleFormData(config.body);
+    try {
+        if (responseType === 'json' || contentType.includes('application/json')) {
+            data = await response.json();
+        } else if (responseType === 'text') {
+            data = await response.text();
+        } else if (responseType === 'blob') {
+            data = await response.blob();
+        } else if (responseType === 'arrayBuffer') {
+            data = await response.arrayBuffer();
         } else {
-            config.body = handleRequestData(config.body);
+            data = response;
         }
-
-        // Retry logic
-        const requestFn = () => this.adapter(config);
-        const response = await addRetryCapability(config, requestFn);
-
-        // Handle response transformation
-        const transformedResponse = await handleResponseData(response, config.responseType);
-
-        // Run response interceptors
-        return this.interceptors.response.run(transformedResponse);
+    } catch (error) {
+        throw new Error(`Failed to parse response as ${responseType || contentType}: ${error.message}`);
     }
 
-    get(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceGet.call(this, url, config);
+    if (transformResponse) {
+        return transformResponse(data);
     }
 
-    post(url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
-        return InstanceMethods.instancePost.call(this, url, data, config);
-    }
-
-    put(url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
-        return InstanceMethods.instancePut.call(this, url, data, config);
-    }
-
-    delete(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceDelete.call(this, url, config);
-    }
-
-    patch(url: string, data: any, config: ValpreAPIConfig = {}): Promise<Response> {
-        return InstanceMethods.instancePatch.call(this, url, data, config);
-    }
-
-    head(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceHead.call(this, url, config);
-    }
-
-    options(url: string, config: ValpreAPIConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceOptions.call(this, url, config);
-    }
-
-    static setDefaults(newDefaults: Partial<ValpreAPIConfig>): void {
-        UtilityMethods.setGlobalDefaults(newDefaults);
-    }
-
-    static create(instanceConfig: ValpreAPIConfig): ValpreAPI {
-        return UtilityMethods.createInstance(instanceConfig);
-    }
-
-    static CancelToken = CancelToken;
-
-    static isValpreAPIError(error: any): error is ValpreAPIError {
-        return UtilityMethods.isValpreAPIError(error);
-    }
-
-    static all = (promises: Array<Promise<any>>): Promise<any[]> => {
-        return Promise.all(promises);
-    };
-
-    static spread = (callback: Function): (arr: any[]) => any => {
-        return function wrap(arr: any[]) {
-            return callback(...arr);
-        };
-    };
+    return data;
 }
