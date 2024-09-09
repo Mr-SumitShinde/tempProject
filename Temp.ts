4f6cbff04ed9e01d06f90d81f3053069fa7e1a5a
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-09-09 11:17:34 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -3,3 +3,21 @@ npm install --save-dev webpack webpack-cli ts-loader webpack-node-externals
 nx generate @nrwl/web:webpack --project valpre-api
 
 npm install stream-http https-browserify url
+
+const webpack = require('webpack');
+
+module.exports = {
+  // other webpack configurations
+  resolve: {
+    fallback: {
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "url": require.resolve("url/")
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    })
+  ]
+};
