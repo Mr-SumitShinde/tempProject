186c961a31cce12678d0ba174fe89d93f83ea39e
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-09-09 13:05:33 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,97 +1,32 @@
-// valpre-api-services.ts
-import { httpAdapter } from './httpAdapter';
-import { ValpreAPIServicesConfig, defaults } from './config';
-import { InterceptorManager } from './interceptors';
-import { CancelToken } from './cancelToken';
-import * as InstanceMethods from './instanceMethods';
-import * as UtilityMethods from './utilityMethods';
-import { ValpreAPIServicesError } from './utils/errorHandling';
-import { addRetryCapability } from './utils/retry';
-import { handleRequestData, handleResponseData } from './utils/transformData';
-import { applyCSRFToken } from './utils/csurf';
+// instanceMethods.ts
+import { ValpreAPIServicesConfig } from './config';
+import { ValpreAPIServices } from './valpre-api-services';
 
-export class ValpreAPIServices {
-    defaults: ValpreAPIServicesConfig;
-    interceptors: {
-        request: InterceptorManager<ValpreAPIServicesConfig>;
-        response: InterceptorManager<Response>;
-    };
-    private adapter: (config: ValpreAPIServicesConfig) => Promise<Response>;
+export function instanceGet(this: ValpreAPIServices, url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
+    // Call request directly, instead of calling `this.get()`
+    return this.request({ ...config, method: 'GET', url });
+}
 
-    constructor(config: ValpreAPIServicesConfig = {}, adapter?: (config: ValpreAPIServicesConfig) => Promise<Response>) {
-        this.defaults = { ...defaults, ...config };
-        this.adapter = adapter || httpAdapter;
-        this.interceptors = {
-            request: new InterceptorManager<ValpreAPIServicesConfig>(),
-            response: new InterceptorManager<Response>(),
-        };
-    }
+export function instancePost(this: ValpreAPIServices, url: string, data: any, config: ValpreAPIServicesConfig = {}): Promise<Response> {
+    return this.request({ ...config, method: 'POST', url, body: data });
+}
 
-    async request(config: ValpreAPIServicesConfig): Promise<Response> {
-        config = { ...this.defaults, ...config };
-        config = await this.interceptors.request.run(config);
+export function instancePut(this: ValpreAPIServices, url: string, data: any, config: ValpreAPIServicesConfig = {}): Promise<Response> {
+    return this.request({ ...config, method: 'PUT', url, body: data });
+}
 
-        applyCSRFToken(config);
-        config.body = handleRequestData(config.body, config.headers as Record<string, string>, config.transformRequest);
+export function instanceDelete(this: ValpreAPIServices, url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
+    return this.request({ ...config, method: 'DELETE', url });
+}
 
-        const requestFn = () => this.adapter(config);
-        const response = await addRetryCapability(config, requestFn);
+export function instancePatch(this: ValpreAPIServices, url: string, data: any, config: ValpreAPIServicesConfig = {}): Promise<Response> {
+    return this.request({ ...config, method: 'PATCH', url, body: data });
+}
 
-        const transformedResponse = await handleResponseData(response, config.responseType, config.transformResponse);
-        return this.interceptors.response.run(transformedResponse);
-    }
+export function instanceHead(this: ValpreAPIServices, url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
+    return this.request({ ...config, method: 'HEAD', url });
+}
 
-    // Corrected methods
-    get(url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceGet.call(this, url, config);
-    }
-
-    post(url: string, data: any, config: ValpreAPIServicesConfig = {}): Promise<Response> {
-        return InstanceMethods.instancePost.call(this, url, data, config);
-    }
-
-    put(url: string, data: any, config: ValpreAPIServicesConfig = {}): Promise<Response> {
-        return InstanceMethods.instancePut.call(this, url, data, config);
-    }
-
-    delete(url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceDelete.call(this, url, config);
-    }
-
-    patch(url: string, data: any, config: ValpreAPIServicesConfig = {}): Promise<Response> {
-        return InstanceMethods.instancePatch.call(this, url, data, config);
-    }
-
-    head(url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceHead.call(this, url, config);
-    }
-
-    options(url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
-        return InstanceMethods.instanceOptions.call(this, url, config);
-    }
-
-    // Utility methods
-    static setDefaults(newDefaults: Partial<ValpreAPIServicesConfig>): void {
-        UtilityMethods.setGlobalDefaults(newDefaults);
-    }
-
-    static create(instanceConfig: ValpreAPIServicesConfig): ValpreAPIServices {
-        return UtilityMethods.createInstance(instanceConfig);
-    }
-
-    static CancelToken = CancelToken;
-
-    static isValpreAPIServicesError(error: any): error is ValpreAPIServicesError {
-        return UtilityMethods.isValpreAPIServicesError(error);
-    }
-
-    static all(promises: Array<Promise<any>>): Promise<any[]> {
-        return Promise.all(promises);
-    }
-
-    static spread(callback: Function): (arr: any[]) => any {
-        return function wrap(arr: any[]) {
-            return callback(...arr);
-        };
-    }
+export function instanceOptions(this: ValpreAPIServices, url: string, config: ValpreAPIServicesConfig = {}): Promise<Response> {
+    return this.request({ ...config, method: 'OPTIONS', url });
 }
