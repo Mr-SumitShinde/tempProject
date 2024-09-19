9f2ee73d34fe2412d99bdb5b9d10f056e8aa0f1d
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-09-19 10:14:53 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,59 +1,87 @@
-import { useEffect } from 'react';
-import { useFormContext, FieldValues } from 'react-hook-form';
-
-interface VisibleIf {
-  key: string;
-  value: string | number | boolean | string[];
-}
-
-const useFormWatch = (
-  visibleIf: VisibleIf[] | undefined,
-  name: string,
-  formElementOptions: any
-) => {
-  const { register, watch, unregister, setValue } = useFormContext<FieldValues>();
-
-  if (!visibleIf || visibleIf.length === 0) {
-    useEffect(() => {
-      unregister(name);
-    }, [unregister, name]);
-
-    return true;
-  }
-
-  const watchFields = visibleIf.map(item => item.key);
-  const watchValues = visibleIf.map(item => item.value);
-
-  const watchedValues = watch(watchFields);
-  let isVisible = true;
-
-  if (watchFields.length && watchValues.length) {
-    isVisible = watchFields.every((field, index) => {
-      const expectedValue = watchValues[index];
-      const fieldValue = watchedValues[index];
-
-      // Check if the value is a Set
-      if (fieldValue instanceof Set) {
-        return fieldValue.has(expectedValue);
-      }
-
-      // If not a Set, handle arrays or single values
-      return Array.isArray(fieldValue)
-        ? fieldValue.includes(expectedValue)
-        : fieldValue === expectedValue;
-    });
-  }
-
-  useEffect(() => {
-    if (isVisible) {
-      unregister(name);
-    } else {
-      register(name, formElementOptions);
-      setValue(name, undefined); // Clear the value when the field becomes invisible
+[
+  {
+    "type": "radio",
+    "name": "question1",
+    "label": "Question 1",
+    "options": ["Yes", "No"]
+  },
+  {
+    "type": "radio",
+    "name": "question2",
+    "label": "Question 2",
+    "options": ["A", "B"]
+  },
+  {
+    "type": "checkbox",
+    "name": "question3",
+    "label": "Question 3",
+    "options": ["Option1", "Option2"]
+  },
+  {
+    "type": "input",
+    "name": "question4",
+    "label": "Question 4",
+    "visibleIf": {
+      "and": [
+        {
+          "key": "question1",
+          "value": "Yes"
+        },
+        {
+          "key": "question2",
+          "value": "A"
+        }
+      ]
     }
-  }, [register, unregister, isVisible, name, formElementOptions, setValue]);
-
-  return isVisible;
-};
-
-export { useFormWatch };
+  },
+  {
+    "type": "input",
+    "name": "question5",
+    "label": "Question 5",
+    "visibleIf": {
+      "or": [
+        {
+          "and": [
+            {
+              "key": "question1",
+              "value": "Yes"
+            },
+            {
+              "key": "question3",
+              "value": "Option1"
+            }
+          ]
+        },
+        {
+          "key": "question2",
+          "value": "B"
+        }
+      ]
+    }
+  },
+  {
+    "type": "input",
+    "name": "question6",
+    "label": "Question 6",
+    "visibleIf": {
+      "and": [
+        {
+          "key": "question1",
+          "value": "No"
+        },
+        {
+          "or": [
+            {
+              "key": "question2",
+              "value": "B"
+            },
+            {
+              "key": "question3",
+              "value": "Option2"
+            }
+          ]
+        }
+      ]
+    }
+  }
+]
