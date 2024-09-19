b75f26b108af206293796ff60948369908c4510a
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-09-19 10:28:36 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,87 +1,71 @@
-[
-  {
-    "type": "radio",
-    "name": "question1",
-    "label": "Question 1",
-    "options": ["Yes", "No"]
-  },
-  {
-    "type": "radio",
-    "name": "question2",
-    "label": "Question 2",
-    "options": ["A", "B"]
-  },
-  {
-    "type": "checkbox",
-    "name": "question3",
-    "label": "Question 3",
-    "options": ["Option1", "Option2"]
-  },
-  {
-    "type": "input",
-    "name": "question4",
-    "label": "Question 4",
-    "visibleIf": {
-      "and": [
-        {
-          "key": "question1",
-          "value": "Yes"
-        },
-        {
-          "key": "question2",
-          "value": "A"
-        }
-      ]
-    }
-  },
-  {
-    "type": "input",
-    "name": "question5",
-    "label": "Question 5",
-    "visibleIf": {
-      "or": [
-        {
-          "and": [
-            {
-              "key": "question1",
-              "value": "Yes"
-            },
-            {
-              "key": "question3",
-              "value": "Option1"
-            }
-          ]
-        },
-        {
-          "key": "question2",
-          "value": "B"
-        }
-      ]
-    }
-  },
-  {
-    "type": "input",
-    "name": "question6",
-    "label": "Question 6",
-    "visibleIf": {
-      "and": [
-        {
-          "key": "question1",
-          "value": "No"
-        },
-        {
-          "or": [
-            {
-              "key": "question2",
-              "value": "B"
-            },
-            {
-              "key": "question3",
-              "value": "Option2"
-            }
-          ]
-        }
-      ]
-    }
+import { useEffect } from 'react';
+import { useFormContext, FieldValues } from 'react-hook-form';
+
+interface VisibleIfCondition {
+  key?: string;
+  value?: string | number | boolean | string[];
+  and?: VisibleIfCondition[];
+  or?: VisibleIfCondition[];
+}
+
+const useFormWatch = (
+  visibleIf: VisibleIfCondition[] | undefined,
+  name: string,
+  formElementOptions: any
+) => {
+  const { register, watch, unregister, setValue } = useFormContext<FieldValues>();
+
+  if (!visibleIf || visibleIf.length === 0) {
+    useEffect(() => {
+      unregister(name);
+    }, [unregister, name]);
+
+    return true;
   }
-]
+
+  const watchFields = visibleIf.flatMap(item => collectWatchFields(item));
+  const watchedValues = watch(watchFields);
+
+  const evaluateCondition = (condition: VisibleIfCondition): boolean => {
+    if (condition.and) {
+      return condition.and.every(subCondition => evaluateCondition(subCondition));
+    }
+    if (condition.or) {
+      return condition.or.some(subCondition => evaluateCondition(subCondition));
+    }
+
+    const { key, value } = condition;
+    const fieldValue = watchedValues[watchFields.indexOf(key)];
+
+    if (fieldValue instanceof Set) {
+      return fieldValue.has(value);
+    }
+
+    return Array.isArray(fieldValue) ? fieldValue.includes(value) : fieldValue === value;
+  };
+
+  let isVisible = visibleIf.every(condition => evaluateCondition(condition));
+
+  function collectWatchFields(condition: VisibleIfCondition): string[] {
+    if (condition.and) {
+      return condition.and.flatMap(subCondition => collectWatchFields(subCondition));
+    }
+    if (condition.or) {
+      return condition.or.flatMap(subCondition => collectWatchFields(subCondition));
+    }
+    return [condition.key!];
+  }
+
+  useEffect(() => {
+    if (isVisible) {
+      unregister(name);
+    } else {
+      register(name, formElementOptions);
+      setValue(name, null);
+    }
+  }, [register, unregister, isVisible, name, formElementOptions, setValue]);
+
+  return isVisible;
+};
+
+export { useFormWatch, VisibleIfCondition };
