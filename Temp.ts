0738ec231b959015ab1997bcc25b135d621ebb30
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-10-14 10:00:19 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -14,3 +14,8 @@ sonar.test.inclusions=**/*.spec.ts,**/*.test.ts
 sonar.javascript.lcov.reportPaths=coverage/lcov.info
 sonar.projectBaseDir=.
 sonar.sourceEncoding=UTF-8
+
+
+In an Nx monorepo, when the jest.config.ts file is created, it typically exports a function from @nx/jest. This function is not explicitly imported anywhere within your application code, but it's used internally by Nx to set up Jest for the particular project.
+
+The configuration file (jest.config.ts) is automatically consumed when you run nx test <project-name>. Nx uses the Jest configuration when running your tests and applies any custom settings or configurations specified in that file.
