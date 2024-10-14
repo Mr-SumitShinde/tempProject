a5a828e748fc763862a789f74863dacc79b41203
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-10-14 09:50:48 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -3,12 +3,12 @@ sonar.organization=your_organization_key
 sonar.projectName=Your Project Name
 sonar.projectVersion=1.0
 
-sonar.sources=apps,your-libraries-folder
+sonar.sources=apps,@barclays/libs
 sonar.language=ts
 sonar.typescript.tsconfigPath=tsconfig.base.json
 sonar.exclusions=**/node_modules/**, **/dist/**
 
-sonar.tests=apps,your-libraries-folder
+sonar.tests=apps,@barclays/libs
 sonar.test.inclusions=**/*.spec.ts,**/*.test.ts
 
 sonar.javascript.lcov.reportPaths=coverage/lcov.info
