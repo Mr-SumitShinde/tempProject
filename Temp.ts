c867f22591b67a8196b91042ec63eb73b958c4d8
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-10-30 14:39:25 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,27 +1,26 @@
-npm install --save-dev @babel/preset-env @babel/preset-react @babel/preset-typescript babel-jest
-
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(d3)/)',
-  ],
-  moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy',
-  },
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-};
-
-
-{
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript"
-  ]
+function addFilterProperties(columnDefs) {
+  return columnDefs.map(column => {
+    if (column.isFilter) {
+      return {
+        ...column,
+        filter: 'agTextColumnFilter',
+        filterParams: {
+          filterOptions: ["contains"],
+          maxNumConditions: 1
+        }
+      };
+    }
+    return column; // return column as is if isFilter is not true
+  });
 }
+
+// Example usage
+const columnDefs = [
+  { field: 'name', isFilter: true },
+  { field: 'age' },
+  { field: 'country', isFilter: true },
+  { field: 'status' }
+];
+
+const updatedColumnDefs = addFilterProperties(columnDefs);
+console.log(updatedColumnDefs);
