e3459435ec7124a65c49b9fc50ff46b72ef5624e
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-06 16:37:21 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,7 +1,65 @@
-function modifyUrl(url: string): string {
-  return url.includes('?') ? url.replace(/&?$/, '&') : `${url}?`;
-}
+import React, { useState } from 'react';
+import {
+  PaginationSimple,
+  Section,
+  SectionItem,
+  Table,
+  Type
+} from '@barclays/blueprint-react';
 
-// Usage example
-console.log(modifyUrl("https://example.com?param=value")); // Outputs: https://example.com?param=value&
-console.log(modifyUrl("https://example.com"));             // Outputs: https://example.com?
+const ValpreReactDataTable = ({ data, onPageChange, totalPages, headers }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Handle page change
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    onPageChange(newPage);
+  };
+
+  const renderCellContent = (item, header) => {
+    // If a render function is provided, use it to render the content
+    if (header.render) {
+      return header.render(item);
+    }
+    // Otherwise, return the data directly
+    return item[header.dataKey];
+  };
+
+  return (
+    <Section>
+      <SectionItem>
+        <Table headingVariant="secondary" tableHeadNoPaddingBottom>
+          <thead>
+            <tr>
+              {headers.map((header, index) => (
+                <Type key={index} alignment={header.alignment} element="th" scope="col" weight={header.weight}>
+                  {header.title}
+                </Type>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                {headers.map((header, idx) => (
+                  <Type key={idx} alignment={header.alignment} element="td" weight={header.weight}>
+                    {renderCellContent(item, header)}
+                  </Type>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </SectionItem>
+      <SectionItem>
+        <PaginationSimple
+          active={currentPage}
+          onButtonClick={handlePageChange}
+          total={totalPages}
+        />
+      </SectionItem>
+    </Section>
+  );
+};
+
+export default ValpreReactDataTable;
