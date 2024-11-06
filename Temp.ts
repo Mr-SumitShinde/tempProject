dcbb5a268143ab5117537dabef27a756b7382ebe
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-06 23:59:04 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,7 +1,9 @@
-const mockData = Array.from({ length: 100 }, (_, index) => ({
-    clientName: `Client ${index + 1}`,
-    status: ['READY TO SEND', 'APPROVED', 'EXPIRED LINK', 'REJECTED', 'REVIEW'][index % 5],
-    requestNo: `PB${2665000 + index}`,
-    submittedBy: ['Rebecca O’Connell', 'Andrew Stocks'][index % 2],
-    dateCreated: `${new Date(2024, 8, (index % 30) + 1).toLocaleDateString('en-US')}`
-}));
+<ValpreReactDataTable
+    baseUrl="http://localhost:3000/data"
+    createQueryParams={(page, pageSize) => `page=${page}&pageSize=${pageSize}`}
+    headers={headers}
+    initialPage={1}
+    pageSize={10}
+    extractDataFromResponse={(responseData) => responseData.items}
+    extractTotalRecordsFromResponse={(responseData) => responseData.totalCount}
+/>
