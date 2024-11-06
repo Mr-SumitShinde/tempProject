b1226cb3292a0e023e629ed2a9276b968d6a258b
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-06 20:45:33 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,71 +1,79 @@
-Showing 1-10 of 99 items as of 01/11/2024 09:00 AM (GMT+1)
-const sampleData = [
-  {
-    id: 1,
-    date: '2024-11-01',
-    description: 'Subscription Fee',
-    moneyIn: 120,
-    moneyOut: 0,
-    balance: 5020,
-  },
-  {
-    id: 2,
-    date: '2024-11-02',
-    description: 'Coffee Purchase',
-    moneyIn: 0,
-    moneyOut: 5,
-    balance: 5015,
-  },
-];
+import React, { useState } from 'react';
+import { Box, PaginationSimple, Section, SectionItem, Table, Type } from '@barclays/blueprint-react';
 
+const ValpreReactDataTable = ({ data, onPageChange, totalPages, headers }) => {
+    const [currentPage, setCurrentPage] = useState(1);
 
-const MoneyComponent = ({ amount }) => (
-  <span style={{ color: amount > 0 ? 'green' : 'red' }}>
-    {amount > 0 ? `+${amount}` : amount}
-  </span>
-);
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+        onPageChange(newPage);
+    };
 
+    const renderCellContent = (item, header) => {
+        if (header.render) {
+            return header.render(item);
+        }
+        return item[header.dataKey];
+    };
 
-const headers = [
-  { title: 'Date', dataKey: 'date', alignment: 'left' },
-  { title: 'Description', dataKey: 'description', alignment: 'left' },
-  {
-    title: 'Money In',
-    dataKey: 'moneyIn',
-    alignment: 'left',
-    render: (item) => item.moneyIn !== 0 && <MoneyComponent amount={item.moneyIn} />,
-  },
-  {
-    title: 'Money Out',
-    dataKey: 'moneyOut',
-    alignment: 'left',
-    render: (item) => item.moneyOut !== 0 && <MoneyComponent amount={-item.moneyOut} />,
-  },
-  { title: 'Balance', dataKey: 'balance', alignment: 'right' },
-];
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ValpreReactDataTable from './ValpreReactDataTable'; // Assume the component is in this file
-
-const App = () => {
-  const handlePageChange = (page) => {
-    console.log('Page changed to:', page);
-    // Here you could fetch new data based on the page number
-  };
-
-  return (
-    <div>
-      <ValpreReactDataTable
-        data={sampleData}
-        onPageChange={handlePageChange}
-        totalPages={5}
-        headers={headers}
-      />
-    </div>
-  );
+    return (
+        <Section>
+            <SectionItem>
+                <Table headingVariant="secondary" tableHeadNoPaddingBottom>
+                    <thead>
+                        <tr>
+                            {headers.map((header, index) => (
+                                <Type
+                                    key={index}
+                                    alignment={header.alignment}
+                                    element="th"
+                                    scope="col"
+                                    weight={header.weight}
+                                >
+                                    {header.title}
+                                </Type>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => (
+                            <tr key={index}>
+                                {headers.map((header, idx) => (
+                                    <Type
+                                        key={idx}
+                                        alignment={header.alignment}
+                                        element="td"
+                                        weight={header.weight}
+                                    >
+                                        {renderCellContent(item, header)}
+                                    </Type>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </SectionItem>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box>
+                    <Type>
+                        Showing 1-10 of 99 items as of 01/11/2024 09:00 AM (GMT+1)
+                    </Type>
+                </Box>
+                <SectionItem>
+                    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                        <div style={{ border: '2px solid rgb(226, 226, 226)', borderRadius: '28px', width: 'fit-content' }}>
+                            <PaginationSimple
+                                variant="secondary"
+                                active={currentPage}
+                                onButtonClick={handlePageChange}
+                                total={totalPages}
+                            />
+                        </div>
+                    </div>
+                </SectionItem>
+            </div>
+        </Section>
+    );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+export default ValpreReactDataTable;
