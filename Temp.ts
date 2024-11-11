470e4c60ec3ea31c9854704bd5ffe5200207de1b
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-11 19:58:58 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,25 +1,8 @@
 useEffect(() => {
-  if (!hasMounted.current) {
-    hasMounted.current = true;
-  } else {
-    const startItemIndex = currentPage * pageSize;
-    const endItemIndex = startItemIndex + pageSize;
-
-    if (currentPage > allData.lastFetchedPage && allData.items.length < endItemIndex) {
-      let newPage = dbPage;
-      if (currentPage === Math.ceil(allData.totalCount / pageSize)) {
-        newPage = Math.ceil(allData.totalCount / pageSize);
-      } else {
-        newPage = dbPage + 1;
-      }
-
-      if (newPage !== dbPage) {
-        setDbPage(newPage);
-      }
-    }
+  // Initialize something safely
+  if (!initialized) {
+    initialize();
+    setInitialized(true);
   }
-}, [currentPage, pageSize, allData, dbPage]);
-
-useEffect(() => {
-  fetchData(dbPage);
-}, [dbPage]);
+  console.log(currentPage);
+}, [currentPage]);
