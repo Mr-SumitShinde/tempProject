ed41f360f47bc2e706452f0cfb6eb96c85c8106b
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-11 14:03:30 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,1 +1,41 @@
-const initialItems = new Array(totalItemCount).fill(undefined);
+const fetchData = async (dbPage: number) => {
+  const startItemIndex = (dbPage - 1) * offset;
+  const endItemIndex = startItemIndex + offset;
+
+  if (allData.items.length < endItemIndex && dbPage > allData.lastFetchedPage) {
+    setLoading(true);
+    setError(null);
+
+    const queryParams = createQueryParams(dbPage, offset);
+    const url = `${baseUrl}?${queryParams}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      const newItems = extractDataFromResponse(responseData);
+      const updatedItems = [...allData.items];
+
+      for (let i = 0; i < newItems.length; i++) {
+        updatedItems[startItemIndex + i] = newItems[i];
+      }
+
+      setAllData(prev => ({
+        ...prev,
+        items: updatedItems,
+        totalCount: extractTotalRecordsFromResponse(responseData),
+        lastFetchedPage: dbPage,
+      }));
+
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  } else {
+    setLoading(false);
+  }
+}
