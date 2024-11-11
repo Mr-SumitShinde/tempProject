02da63f2841602536a5fcedb0b7ee3cc17a7eec1
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-11 16:51:59 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,41 +1,25 @@
-const fetchData = async (dbPage: number) => {
-  const startItemIndex = (dbPage - 1) * offset;
-  const endItemIndex = startItemIndex + offset;
-
-  if (allData.items.length < endItemIndex && dbPage > allData.lastFetchedPage) {
-    setLoading(true);
-    setError(null);
-
-    const queryParams = createQueryParams(dbPage, offset);
-    const url = `${baseUrl}?${queryParams}`;
-
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const responseData = await response.json();
-      const newItems = extractDataFromResponse(responseData);
-      const updatedItems = [...allData.items];
-
-      for (let i = 0; i < newItems.length; i++) {
-        updatedItems[startItemIndex + i] = newItems[i];
-      }
-
-      setAllData(prev => ({
-        ...prev,
-        items: updatedItems,
-        totalCount: extractTotalRecordsFromResponse(responseData),
-        lastFetchedPage: dbPage,
-      }));
-
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
+useEffect(() => {
+  if (!hasMounted.current) {
+    hasMounted.current = true;
   } else {
-    setLoading(false);
+    const startItemIndex = currentPage * pageSize;
+    const endItemIndex = startItemIndex + pageSize;
+
+    if (currentPage > allData.lastFetchedPage && allData.items.length < endItemIndex) {
+      let newPage = dbPage;
+      if (currentPage === Math.ceil(allData.totalCount / pageSize)) {
+        newPage = Math.ceil(allData.totalCount / pageSize);
+      } else {
+        newPage = dbPage + 1;
+      }
+
+      if (newPage !== dbPage) {
+        setDbPage(newPage);
+      }
+    }
   }
-}
+}, [currentPage, pageSize, allData, dbPage]);
+
+useEffect(() => {
+  fetchData(dbPage);
+}, [dbPage]);
