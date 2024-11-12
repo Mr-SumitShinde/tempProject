8977e8f53853138b91c51753018898a8026a479a
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-12 14:41:40 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -5,46 +5,35 @@ Indexs/src/index.html,
 basestrefs/pbwn/launcher/identity-verification'
 
 
-
 const CustomCaseStatus = (item: any) => {
-    let status = item;
+    if (!item) return null;
 
-    switch (status) {
+    const renderTile = (theme: React.ElementType) => (
+        <theme>
+            <Tile>
+                <Tile.Content>
+                    <Type size="sm">&nbsp;&nbsp;{item}</Type>
+                </Tile.Content>
+            </Tile>
+        </theme>
+    );
+
+    switch (item) {
         case caseStatus.AWAITINGINPUT:
         case caseStatus.PROCESSING:
         case caseStatus.CASECREATED:
-            return (
-                item && (
-                    <BarclaysLegacyMicroThemeInfo>
-                        <Tile>
-                            <Tile.Content>
-                                <Type size="sm">&nbsp;&nbsp;{item}</Type>
-                            </Tile.Content>
-                        </Tile>
-                    </BarclaysLegacyMicroThemeInfo>
-                )
-            );
+            return renderTile(BarclaysLegacyMicroThemeInfo);
 
         case caseStatus.APPROVED:
-            return (
-                item && (
-                    <BarclaysLegacyMicroThemeSuccess>
-                        <Tile>
-                            <Tile.Content>
-                                <Type size="sm">&nbsp;&nbsp;{item}</Type>
-                            </Tile.Content>
-                        </Tile>
-                    </BarclaysLegacyMicroThemeSuccess>
-                )
-            );
+            return renderTile(BarclaysLegacyMicroThemeSuccess);
 
         case caseStatus.ERROR:
         case caseStatus.ABANDONED:
         case caseStatus.LINKEXPIRED:
-            // Add your specific return or handling logic here if needed
+            // Add specific handling logic or a return statement here if necessary
             break;
 
         default:
-            return null; // Default return if none of the cases match
+            return null;
     }
 };
