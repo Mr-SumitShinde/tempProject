84cbc4dca64ee577b12456dfb595d0d6dafb0c65
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-14 14:00:33 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,45 +1,11 @@
-type Category = {
-    id: string;
-    type: string;
-    code: string;
-    desc: string;
-    flow: string;
-    subCategories?: {
-        id: string;
-        type: string;
-        code: string;
-        desc: string;
-        flow: string;
-    }[];
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AppProvider } from './AppContext';
 
-type ApiResponse = {
-    data: {
-        id: string;
-        type: string;
-        attributes: {
-            categories: Category[];
-        };
-    };
-};
-
-function processRefData(response: ApiResponse): Record<string, { code: string; desc: string }[]> {
-    const result: Record<string, { code: string; desc: string }[]> = {};
-
-    response.data.attributes.categories.forEach((category) => {
-        const key = category.code;
-
-        if (category.subCategories && category.subCategories.length > 0) {
-            result[key] = category.subCategories.map((subCat) => ({
-                code: subCat.code,
-                desc: subCat.desc,
-            }));
-        } else {
-            result[key] = [{ code: category.code, desc: category.desc }];
-        }
-    });
-
-    return result;
-}
-
-export default processRefData;
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <AppProvider>
+        <App />
+    </AppProvider>
+);
