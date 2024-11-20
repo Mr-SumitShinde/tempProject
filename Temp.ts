1049c6a8a419b4377c4c257817ceef56f7ad4b82
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-21 02:11:50 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,11 +1,5 @@
 useEffect(() => {
-  setStatusList([]);
-
   let arrStatusList = context?.data?.['CSTATUS'] ? [...context.data['CSTATUS']] : [];
-
-  if (!arrStatusList.find(item => item.key === "" && item.text === "")) {
-    arrStatusList.unshift({ key: "", text: "" });
-  }
-
+  arrStatusList = [{ key: "", text: "" }, ...arrStatusList.filter(item => item.key || item.text)];
   setStatusList(arrStatusList);
-}, [context?.data]); // Now the effect runs whenever `context?.data` changes.
+}, []);
