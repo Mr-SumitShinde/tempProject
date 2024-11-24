30979e486e16c9a0d9cf1ec7e02c67a13fe2d922
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-24 12:29:06 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -3,3 +3,8 @@ import { valpreAPIGet } from 'valpre-api-services';
 jest.mock('valpre-api-services', () => ({
   valpreAPIGet: jest.fn(() => Promise.reject(new Error('new error'))),
 }));
+
+
+import * as ValpreAPI from 'valpre-api-services';
+
+jest.spyOn(ValpreAPI, 'valpreAPIGet').mockImplementation(() => Promise.reject(new Error('new error')));
