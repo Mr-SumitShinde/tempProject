a604df9aa40fc2a0d4fe4871077d595e4468d997
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-25 09:47:19 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,23 +1,3 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { ErrorHeader } from './ErrorHeader';
-import { HeroAlert, Button, Icon, Type } from '@barclays/blueprint-react';
-
-jest.mock('@barclays/blueprint-react', () => ({
-  HeroAlert: jest.fn(({ children }: { children: React.ReactNode }) => <div data-testid="hero-alert">{children}</div>),
-  Button: jest.fn(({ children }: { children: React.ReactNode }) => <button>{children}</button>),
-  Icon: jest.fn(() => <span data-testid="icon" />),
-  Type: jest.fn(({ children }: { children: React.ReactNode }) => <div>{children}</div>)
+jest.mock('valpre-api-services', () => ({
+  valpreAPIGet: jest.fn(() => Promise.reject(new Error('new error'))),
 }));
-
-describe('ErrorHeader', () => {
-  it('renders the HeroAlert with appropriate content', () => {
-    render(<ErrorHeader />);
-
-    expect(screen.getByTestId('hero-alert')).toBeInTheDocument();
-    expect(screen.getByTestId('icon')).toBeInTheDocument();
-    expect(screen.getByText('Sorry, something went wrong')).toBeInTheDocument();
-    expect(screen.getByText('Please try after some time')).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Button label' })).toBeInTheDocument();
-  });
-});
