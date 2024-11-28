a1fc1f85e47858365724b5b44db4880b85182fbd
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-11-28 15:08:51 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,23 +1,34 @@
-useEffect(() => {
-  const fetchData = async () => {
-    const queryParams = { refreshStatus: 'Y', generateLink: 'N' };
+const handleGenLink = async () => {
+  if (casestatus === 'REVIEW' || casestatus === 'APPROVED') {
+    const url = `${baseurl}/${caseDetailsData?.data?.attributes?.caseDetails?.caseId}/documents`;
+
+    valpreAPIGet(url, requestOptions)
+      .then(async (response) => {
+        await setReportData(response);
+        await pdfGenerator(
+          response?.data?.attributes?.file,
+          caseDetailsData?.data?.attributes?.caseDetails?.caseId
+        );
+      })
+      .catch((error) => {
+        window.scrollTo(0, 0);
+        setErrorFlag(true);
+        console.error('Error downloading PDF file', error);
+        throw error;
+      });
+  } else {
+    const queryParams = { refreshStatus: 'Y', generateLink: 'Y' };
     const url = urlGen(queryParams);
 
     valpreAPIGet(url, requestOptions)
       .then(async (response) => {
         await setCaseDetailsData(response);
-
-        if (caseStatus === 'AWAITING INPUT') {
-          setGenlinkFlag(true);
-        }
-
+        setGenlinkFlag(true);
         window.scrollTo(0, document.body.scrollHeight);
       })
       .catch((error) => {
         console.error('Error fetching data', error);
         setErrorFlag(true);
       });
-  };
-
-  fetchData();
-}, []);
+  }
+};
