c2bdb07f6bc622bd37689a5d276e80991086fd9e
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2024-12-16 06:26:44 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,3 +1,28 @@
-git reset --hard HEAD~2
+import React from 'react';
 
-git push origin <branch-name> --force
+interface TableBodyProps<T> {
+  data: T[];
+  headers: Array<{ datakey: keyof T; alignment?: 'left' | 'center' | 'right'; render?: (item: T) => JSX.Element }>;
+}
+
+export const TableBody = <T,>({ data, headers }: TableBodyProps<T>) => (
+  <tbody>
+    {data.length > 0 ? (
+      data.map((item, rowIndex) => (
+        <tr key={rowIndex}>
+          {headers.map((header, colIndex) => (
+            <td key={colIndex} style={{ textAlign: header.alignment || 'left' }}>
+              {header.render ? header.render(item) : item[header.datakey]?.toString()}
+            </td>
+          ))}
+        </tr>
+      ))
+    ) : (
+      <tr>
+        <td colSpan={headers.length} style={{ textAlign: 'center' }}>
+          No data available
+        </td>
+      </tr>
+    )}
+  </tbody>
+);
