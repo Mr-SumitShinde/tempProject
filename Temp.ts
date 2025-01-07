9335509e4e9f74468b9d552c4bf8974e7453b3b6
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2025-01-07 12:31:59 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,36 +1,20 @@
-export interface ValpreReactDataTableCSRProps<T> {
-  renderMode: 'CSR';
-  headers: Header<T>[];
-  data: T[];
-  showSearch?: boolean;
-  pageSize?: number;
-  defaultSortKey?: string;
-  defaultSortDirection?: 'asc' | 'desc';
-  onSortChange?: (sortKey: string, sortDirection: 'asc' | 'desc') => void;
+import React from 'react';
+import { ServerSideDataTable } from './ServerSideDataTable';
+import { ClientSideDataTable } from './ClientSideDataTable';
+import { ValpreReactDataTableProps } from './interfaces';
+
+export function ValpreReactDataTable<T extends object>(
+  props: ValpreReactDataTableProps<T>
+) {
+  if (props.renderMode === 'CSR') {
+    const { renderMode, ...rest } = props;
+    return <ClientSideDataTable<T> {...rest} />;
+  }
+
+  if (props.renderMode === 'SSR') {
+    const { renderMode, ...rest } = props;
+    return <ServerSideDataTable<T> {...rest} />;
+  }
+
+  return null;
 }
-
-export interface ValpreReactDataTableSSRProps<T> {
-  renderMode: 'SSR';
-  headers: Header<T>[];
-  baseUrl: string;
-  createQueryParams: (
-    page: number,
-    offset: number,
-    sortKey?: string,
-    sortDirection?: 'asc' | 'desc',
-    searchQuery?: string
-  ) => string;
-  extractDataFromResponse: (response: any) => T[];
-  extractTotalRecordsFromResponse: (response: any) => number;
-  extractTimeFromResponse?: (response: any) => string;
-  showSearch?: boolean;
-  pageSize?: number;
-  defaultSortKey?: string;
-  defaultSortDirection?: 'asc' | 'desc';
-  onSortChange?: (sortKey: string, sortDirection: 'asc' | 'desc') => void;
-}
-
-
-export type ValpreReactDataTableProps<T> =
-  | ValpreReactDataTableCSRProps<T>
-  | ValpreReactDataTableSSRProps<T>;
