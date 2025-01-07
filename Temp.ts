2d6acf73cd542be41cdf27165449167fd9f523ba
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2025-01-07 12:16:50 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,18 +1,32 @@
 import React from 'react';
-import { ServerSideDataTable } from './ServerSideDataTable';
-import { ClientSideDataTable } from './ClientSideDataTable';
 import { ValpreReactDataTableProps } from './interfaces';
 
-export function ValpreReactDataTable<T>({
-  renderMode,
-  data,
-  ...rest
-}: ValpreReactDataTableProps<T>) {
-  if (renderMode === 'CSR') {
-    if (!data) {
-      throw new Error('Data is required for client-side rendering (CSR).');
-    }
-    return <ClientSideDataTable data={data} {...rest} />;
-  }
-  return <ServerSideDataTable {...rest} />;
+export function ClientSideDataTable<T>({
+  headers,
+  showSearch,
+  pageSize = 10,
+  data: initialData,
+}: Omit<ValpreReactDataTableProps<T>, 'renderMode' | 'baseUrl' | 'createQueryParams' | 'extractDataFromResponse' | 'extractTotalRecordsFromResponse' | 'extractTimeFromResponse'> & { data: T[] }) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header.title}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {initialData.map((row, index) => (
+            <tr key={index}>
+              {headers.map((header, colIndex) => (
+                <td key={colIndex}>{row[header.datakey]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
