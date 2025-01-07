8b56645be432cbef9c729b6301b9d3a0db4d7320
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2025-01-07 11:58:04 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,14 +1,28 @@
-import React from 'react';
-import { ServerSideDataTable } from './ServerSideDataTable';
-import { ClientSideDataTable } from './ClientSideDataTable';
-import { ValpreReactDataTableProps } from './interfaces';
+export interface ValpreReactDataTableProps<T> {
+  renderMode: 'SSR' | 'CSR';
+  headers: Header<T>[];
+  showSearch?: boolean;
+  pageSize?: number;
+  defaultSortKey?: string;
+  defaultSortDirection?: 'asc' | 'desc';
+  onSortChange?: (sortKey: string, sortDirection: 'asc' | 'desc') => void;
+  baseUrl?: string;
+  createQueryParams?: (
+    page: number,
+    offset: number,
+    sortKey?: string,
+    sortDirection?: 'asc' | 'desc',
+    searchQuery?: string
+  ) => string;
+  extractDataFromResponse?: (response: any) => T[];
+  extractTotalRecordsFromResponse?: (response: any) => number;
+  extractTimeFromResponse?: (response: any) => string;
+  data?: T[];
+}
 
-export function ValpreReactDataTable<T>(props: ValpreReactDataTableProps<T>) {
-  const { renderMode, ...rest } = props;
-
-  if (renderMode === 'SSR') {
-    return <ServerSideDataTable {...rest} />;
-  }
-
-  return <ClientSideDataTable {...rest} />;
+export interface Header<T> {
+  title: string;
+  dataKey: keyof T;
+  sortable?: boolean;
+  alignment?: 'left' | 'center' | 'right';
 }
