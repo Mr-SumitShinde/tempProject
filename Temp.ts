ba10522958afe927b93e075695077120fe915ed2
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2025-01-07 12:26:12 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,91 +1,31 @@
-import React from 'react';
-import { ValpreReactDataTableProps } from './interfaces';
-
-export function ClientSideDataTable<T>({
-  headers,
-  showSearch,
-  pageSize = 10,
-  data: initialData,
-}: Omit<ValpreReactDataTableProps<T>, 'renderMode' | 'baseUrl' | 'createQueryParams' | 'extractDataFromResponse' | 'extractTotalRecordsFromResponse' | 'extractTimeFromResponse'> & { data: T[] }) {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            {headers.map((header, index) => (
-              <th key={index}>{header.title}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {initialData.map((row, index) => (
-            <tr key={index}>
-              {headers.map((header, colIndex) => (
-                <td key={colIndex}>{row[header.datakey]}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+export interface ValpreReactDataTableCSRProps<T> {
+  renderMode: 'CSR';
+  headers: Header<T>[];
+  data: T[];
+  showSearch?: boolean;
+  pageSize?: number;
+  defaultSortKey?: string;
+  defaultSortDirection?: 'asc' | 'desc';
+  onSortChange?: (sortKey: string, sortDirection: 'asc' | 'desc') => void;
 }
 
-
-import React from 'react';
-import { ServerSideDataTable } from './ServerSideDataTable';
-import { ClientSideDataTable } from './ClientSideDataTable';
-import { ValpreReactDataTableProps } from './interfaces';
-
-export function ValpreReactDataTable<T>({
-  renderMode,
-  ...rest
-}: ValpreReactDataTableProps<T>) {
-  if (renderMode === 'CSR') {
-    if (!rest.data) {
-      throw new Error('Data is required for client-side rendering (CSR).');
-    }
-    return <ClientSideDataTable {...rest} data={rest.data} />;
-  }
-
-  return <ServerSideDataTable {...rest} />;
-}
-
-import React from 'react';
-import { ValpreReactDataTableProps } from './interfaces';
-
-export function ServerSideDataTable<T>({
-  headers,
-  baseUrl,
-  createQueryParams,
-  pageSize = 10,
-  extractDataFromResponse,
-  extractTotalRecordsFromResponse,
-  extractTimeFromResponse,
-  showSearch,
-  defaultSortKey,
-  defaultSortDirection,
-}: Omit<
-  ValpreReactDataTableProps<T>,
-  'renderMode' | 'data'
->) {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            {headers.map((header, index) => (
-              <th key={index}>{header.title}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {/* Server-side rendering logic will populate rows */}
-          <tr>
-            <td colSpan={headers.length}>Data fetched from server</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+export interface ValpreReactDataTableSSRProps<T> {
+  renderMode: 'SSR';
+  headers: Header<T>[];
+  baseUrl: string;
+  createQueryParams: (
+    page: number,
+    offset: number,
+    sortKey?: string,
+    sortDirection?: 'asc' | 'desc',
+    searchQuery?: string
+  ) => string;
+  extractDataFromResponse: (response: any) => T[];
+  extractTotalRecordsFromResponse: (response: any) => number;
+  extractTimeFromResponse?: (response: any) => string;
+  showSearch?: boolean;
+  pageSize?: number;
+  defaultSortKey?: string;
+  defaultSortDirection?: 'asc' | 'desc';
+  onSortChange?: (sortKey: string, sortDirection: 'asc' | 'desc') => void;
 }
