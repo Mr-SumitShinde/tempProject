10d6145a34338f4dfcbc3a744d53d39a33ccfe9d
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2025-01-20 21:01:25 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,1 +1,15 @@
 npm install --save-dev jest @testing-library/react @testing-library/jest-dom jest-environment-jsdom @nx/jest
+
+
+/* jest.config.ts */
+export default {
+  displayName: 'root',
+  preset: './jest.preset.js',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], // Optional: for global test setup
+};
