e60ccfc9c1a702ab3cca69254c1d3c6b7752822f
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2025-02-17 16:24:03 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -2,13 +2,23 @@ import { useEffect } from "react";
 
 const LeftNavApp = () => {
   useEffect(() => {
-    window.dispatchEvent(new CustomEvent("leftnav-mounted", { detail: true }));
-    sessionStorage.setItem("leftnav-mounted", "true");
+    const updateState = (mounted: boolean) => {
+      window.leftNavMountedState = mounted;
+      window.dispatchEvent(new CustomEvent("leftnav-mounted", { detail: mounted }));
+    };
+
+    updateState(true);
 
     return () => {
-      window.dispatchEvent(new CustomEvent("leftnav-mounted", { detail: false }));
-      sessionStorage.setItem("leftnav-mounted", "false");
+      updateState(false);
     };
+  }, []);
+
+  useEffect(() => {
+    if (window.leftNavMountedState === undefined) {
+      window.leftNavMountedState = false;
+    }
+    window.dispatchEvent(new CustomEvent("leftnav-mounted", { detail: window.leftNavMountedState }));
   }, []);
 
   return <nav>Left Navigation Menu</nav>;
@@ -16,15 +26,22 @@ const LeftNavApp = () => {
 
 export default LeftNavApp;
 
+
 import { useEffect, useState } from "react";
 
 const Header = () => {
-  const [isLeftNavMounted, setIsLeftNavMounted] = useState(
-    sessionStorage.getItem("leftnav-mounted") === "true"
-  );
+  const getStoredLeftNavState = () => {
+    return window.leftNavMountedState ?? false;
+  };
+
+  const [isLeftNavMounted, setIsLeftNavMounted] = useState(getStoredLeftNavState());
 
   useEffect(() => {
-    const handleLeftNavMountEvent = (event: any) => setIsLeftNavMounted(event.detail);
+    const handleLeftNavMountEvent = (event: any) => {
+      window.leftNavMountedState = event.detail;
+      setIsLeftNavMounted(event.detail);
+    };
+
     window.addEventListener("leftnav-mounted", handleLeftNavMountEvent);
 
     return () => {
