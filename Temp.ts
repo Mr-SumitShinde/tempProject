e4d53c3368b879173c0177ae22057276a270a6ae
sumit <108225266+Mr-SumitShinde@users.noreply.github.com>
2025-10-13 10:13:13 +0530

    Update Temp.ts

--- Temp.ts
+++ Temp.ts
@@ -1,7 +1,1 @@
-1)Prospect belongs to PBUK Business segment.
-
-2)Prospect is onboarding into London Booking Center only.
-
-3)Prospect type is Single New to bank customers and 'ready for onboarding' only with no associated related parties.
-
-4) Prospects mapped to respective Front Office/Banker role only should be pulled into CLM.
+https://valpre-workflow-jbpm-1-dev-30158.np6.apaas4.barclays.intranet/kie-wb.jsp#ProcessDefinitions%7CProcessDefinitionList
